556f5f5c2ef5944795ebde4fc2106c747b96b12b
MystRiim <157691471+MystCely@users.noreply.github.com>
2025-10-20 21:47:25 +0200

    updated logView and css

---
+++ script/logView.js
@@ -0,0 +1,67 @@
+const app = document.getElementById('app');
+
+logView();
+
+function logView () {
+  app.innerHTML = `
+    <div class="main-container">
+    <!--    Add  picture tomorrow-->
+    <!--      <img src="img/back-icon.png" alt="back-icon">-->
+      <h1 class="log-header">Log</h1>
+      <div class="drink-list-container">
+        <ul id="drinkList"></ul>
+        <div class="log-container">
+            <div id="drinkInformation">
+                <p class="choose-drink">Choose a drink</p>
+            </div>
+            <div class="journal-container">
+              <h3 class="journal-header">Journal</h3>
+              <textarea id="journal" cols="30" rows="5" placeholder="How was your day?"></textarea>
+              <h3 class="reflections-header">Reflections</h3>
+              <textarea id="reflections" cols="30" rows="5" placeholder="How are you feeling?"></textarea>
+            </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="log-btn-container">
+      <button id="saveLogBtn" onclick="logDrink()">Add</button>
+    </div>
+    `;
+}
+
+const drinkList = document.getElementById('drinkList');
+const drinkInfo = document.getElementById('drinkInformation');
+
+showDrinkList();
+function showDrinkList () {
+  // display as a list
+  model.data.drinkList.forEach(drink => {
+    const li = document.createElement('li');
+    li.textContent = drink.name;
+    li.classList.add('drink-list-item');
+
+    li.dataset.name = drink.name;
+    li.dataset.size = drink.size;
+    li.dataset.caffein = drink.caffein;
+
+    drinkList.appendChild(li);
+  });
+
+}
+
+// event listener to show chosen drink
+drinkList.addEventListener('click', event => {
+  if (event.target.tagName === 'LI') {
+    const { name, size, caffein } = event.target.dataset;
+    drinkInfo.innerHTML =
+      `<p class="drink-title">${name}</p>
+       <p class="drink-info">Size: ${size}</p>
+       <p class="drink-info">Caffein: ${caffein}</p>`;
+  }
+});
+
+function logDrink () {
+  // function to save log somewhere in the model
+}
+
